0dce115d9704c2ee7e86d9464f2dbb70ff85d405
Leonardo Martes <leonardomartes.1997@gmail.com>
2020-06-15 22:47:47 -0500

    Se carga la rama actividades para ver lo karaokes

---
+++ views/activities.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<meta name="description" content="" />
+		<meta name="author" content="Synergia 4.0" />
+
+		<title>Actividades</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+	</head>
+
+	<body>
+		<nav class="navbar navbar-light bg-light">
+			<div class="container">
+				<a class="navbar-brand" href="../views/home.php">
+					<img src="../src/AMAO.png" width="250" height="61" alt="amao" />
+				</a>
+			</div>
+		</nav>
+
+		<div class="container">
+			<h1 class="mt-5 font-weight-light text-center">Karaoke Musical Caribe Canta</h1>
+			<div class="row">
+				<p class="lead text-50">
+					Amigo de Amao, hoy comenzaremos a vivir una maravillosa experiencia en el mundo de <strong>ORBIS, </strong>
+					es por eso que comenzaremos con nuestra primera actividad, ¡Karaoke musical caribe canta!.
+				</br>En esta actividad podrás mostrar tus dotes musicales y realizar la actividad con compañía
+					de tus padres donde debes hacer un video en formato <strong>mp4</strong> y subir a nuestra
+					plataforma esa evidencia del karaoke musical.
+				<br><small class="text-muted">Nota: Primero debes ver las canciones y luego hacer los karaokes musicales.</small>
+					
+				</p>
+			</div>
+			
+			<!-- Video #1 -->
+			<h2>La piragua</h2>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Musical original</h5>
+							<p class="card-text">¡Apréndete la canción y divierte!</p>
+							<iframe width="500"
+							height="315"
+							src="https://www.youtube.com/embed/D-kupCApwOQ"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Karaoke musical</h5>
+							<p class="card-text">Realiza el karaoke en compañía de tus padres.</p>
+							<iframe width="500"
+							height="315"
+							src="https://www.youtube.com/embed/lkAeKkVzf2Y"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr class="my-4">
+			
+			<!-- Video #2 -->
+			<h2>Se va el caiman</h2>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Musical original</h5>
+							<p class="card-text">¡Apréndete la canción y divierte!</p>
+							<iframe width="500"
+							height="315"
+							src="https://www.youtube.com/embed/__pm9WaE3WQ"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Karaoke musical</h5>
+							<p class="card-text">Realiza el karaoke en compañía de tus padres.</p>
+							<iframe width="500"
+							height="315"
+							src="https://www.youtube.com/embed/x4x-Z5NdWZw"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr class="my-4">
+			
+			<!-- Video #3 -->
+			<h2>Luna de Barranquilla</h2>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Musical original</h5>
+							<p class="card-text">¡Apréndete la canción y divierte!</p>
+							<iframe width="500"
+							height="315"
+							src="https://www.youtube.com/embed/_kYCp3DvZEg"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Karaoke musical</h5>
+							<p class="card-text">Realiza el karaoke en compañía de tus padres.</p>
+							<iframe width="500"
+							height="315"
+							src="https://www.youtube.com/embed/sWHUWkIqZ0U"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr class="my-4">
+
+			<!-- Video #4 -->
+			<h2>La llorona loca cumbia</h2>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Musical original</h5>
+							<p class="card-text">¡Apréndete la canción y divierte!</p>
+							<iframe width="500"
+							height="315"
+							src="https://www.youtube.com/embed/THnSWEjy0ko"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Karaoke musical</h5>
+							<p class="card-text">Realiza el karaoke en compañía de tus padres.</p>
+							<iframe width="500"
+							height="315"
+							src="https://www.youtube.com/embed/mYqcK0V5iHs"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<button type="button" class="btn btn-primary btn-lg btn-block mt-5 mb-10">Agregar karaokes</button>
+		</div>
+
+		<!-- Bootstrap core JavaScript -->
+		<script src="../vendor/jquery/jquery.min.js"></script>
+		<script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	</body>
+</html>
